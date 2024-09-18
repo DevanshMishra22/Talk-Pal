c3f2779235fe836a5c70fbaecb0ec1b64a89beb8
Devansh Mishra <devanshmishra2233@gmail.com>
2024-09-18 12:05:21 +0530

    added component

--- client/src/components/specific/Search.jsx
+++ client/src/components/specific/Search.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import {
+  Dialog,
+  DialogTitle,
+  InputAdornment,
+  List,
+  Stack,
+  TextField,
+} from "@mui/material";
+import { Search as SearchIcon } from "@mui/icons-material";
+import { useInputValidation } from "6pp";
 
+const user = [];
 const Search = () => {
-  return <div>Search</div>;
+  const search = useInputValidation("");
+  return (
+    <Dialog open>
+      <Stack p={"2rem"} direction={"column"} width={"25rem"}>
+        <DialogTitle textAlign={"center"}>Find People</DialogTitle>
+        <TextField
+          label=""
+          value={search.value}
+          onChange={search.changeHandler}
+          variant="outlined"
+          size="small"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        ></TextField>
+
+        <List></List>
+      </Stack>
+    </Dialog>
+  );
 };
 
 export default Search;
