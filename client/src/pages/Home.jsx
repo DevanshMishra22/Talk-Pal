23c897c474e89c33433e5b33819412b09bf63507
Devansh <devansh@singys.com>
2025-05-13 18:24:26 +0530

    home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import AppLayout from "../components/layout/AppLayout";
+import { Box, Typography } from "@mui/material";
 
 const Home = () => {
-  return <div>Home page</div>;
+  return (
+    <Box
+      bgcolor={"#433e3e"}
+      height={"100%"}
+      display={"flex"}
+      alignItems={"center"}
+      justifyContent={"center"}
+    >
+      <Typography variant="h5" color="white" textAlign={"center"}>
+        Select Your friend to chat 😁
+      </Typography>
+    </Box>
+  );
 };
 
 export default AppLayout()(Home);
